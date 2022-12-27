5b76a67fd68f72d4fa460886c7d53492f2c5c2e5
Ingrid Leal <icsleal@outlook.com>
2022-12-27 00:19:30 -0300

    Exercícios

--- Ex3_NumeroAleatorios.java
+++ Ex3_NumeroAleatorios.java
@@ -1,2 +1,40 @@
+import java.util.Random;
+
+/*
+Faça um Programa que leia 20 números inteiros aleátórios (entre 0 e 100) armazen-os num vetor.
+Ao final mostre os números e seus sucessores.
+ */
 public class Ex3_NumeroAleatorios {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatorios = new int[20];
+
+        for(int i = 0; i < numerosAleatorios.length; i++) {
+            int numero = random.nextInt(100);
+            numerosAleatorios[i] = numero;
+        }
+
+        System.out.println("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println(numero + " ");
+        }
+
+        System.out.println("Numeros Aleatorios: ");
+        for (int numero : numerosAleatorios) {
+            System.out.println((numero+1) + " ");
+        }
+    }
+
 }
+
